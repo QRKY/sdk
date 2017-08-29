350f5b408db58696573adcfb0879b3db4136d385
ardroz <darr.0725@gmail.com>
2017-08-29 14:05:38 -0500

    :rocket: Groups functionality added

---
+++ src/groups.js
@@ -0,0 +1,67 @@
+/*jslint node: true, indent:2, regexp:true*/
+'use strict';
+
+module.exports = function (qrk, request) {
+
+  qrk.userGroupsGet = function (spec, cb) {
+    var path;
+
+    spec = spec || {};
+
+    path = '/users/' + (spec.user || '') + '/groups';
+
+    path = spec.group ? (path + '/' + spec.group) : path;
+
+    return request.call(this, {
+      path    : path,
+      method  : 'GET'
+    }, cb);
+  };
+
+  qrk.userGroupsCreate = function (spec, cb) {
+    var path;
+
+    spec = spec || {};
+
+    path = '/users/' + (spec.user || '') + '/groups';
+
+    return request.call(this, {
+      path    : path,
+      method  : 'POST',
+      body    : {
+        name: spec.name || '',
+        members: spec.members || []
+      }
+    }, cb);
+  };
+
+  qrk.userGroupsUpdate = function (spec, cb) {
+    var path;
+
+    spec = spec || {};
+
+    path = '/users/' + (spec.user || '') + '/groups/' + spec.group;
+
+    return request.call(this, {
+      path    : path,
+      method  : 'PUT',
+      body    : {
+        name: spec.name || '',
+        members: spec.members || null
+      }
+    }, cb);
+  };
+
+  qrk.userGroupsRemove = function (spec, cb) {
+    var path;
+
+    spec = spec || {};
+
+    path = '/users/' + (spec.user || '') + '/groups/' + spec.group;
+
+    return request.call(this, {
+      path    : path,
+      method  : 'DELETE'
+    }, cb);
+  };
+};
