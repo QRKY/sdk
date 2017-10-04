8c1d1bc7e847773f2b3d88c788b9d46935d64d35
ardroz <darr.0725@gmail.com>
2017-10-03 21:06:48 -0500

    :rocket: Combos get

---
+++ src/combosGet.js
@@ -0,0 +1,50 @@
+/*jslint node: true, indent:2, regexp:true*/
+'use strict';
+var pick      = require('./lib/pick'),
+  Promise     = global.Promise || require('promise-polyfill');
+
+function defer() {
+  var deferred, promise;
+
+  deferred = {};
+  promise = new Promise(function (resolve, reject) {
+    deferred.resolve = resolve;
+    deferred.reject  = reject;
+  });
+
+  deferred.promise = promise;
+  return deferred;
+}
+
+module.exports = function (httpRequest) {
+  return function (spec, cb) {
+    var opts, deferred = defer();
+    spec = spec || {};
+
+    opts = {
+      host    : spec.domain || 'api.qrk.mx',
+      protocol: spec.protocol || 'https',
+      path    : '/combos',
+      method  : 'GET'
+    };
+
+    httpRequest(opts, function (res) {
+      if (res.statusCode >= 200 && res.statusCode < 300) {
+        if (cb) {
+          cb(null, res.body);
+          return;
+        }
+        deferred.resolve(res.body);
+        return;
+      }
+      if (cb) {
+        cb(res.body, null);
+      }
+      deferred.reject(res.body);
+    });
+
+    if (!cb) {
+      return deferred.promise;
+    }
+  };
+};
